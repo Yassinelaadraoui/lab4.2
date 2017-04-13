4a9a5115ee7a57d6e9b3e512b1ef77f75dedf4b2
Yassinelaadraoui <yassinelaadraoui@gmail.com>
2017-04-14 00:55:03 +0300

    changes

--- MyApp.java
+++ MyApp.java
@@ -4,19 +4,22 @@ public class main {
  		System.out.println("Product value is " + product.countValue());
  		product.printProduct();
  	}
+   public void changePrice() {
+    
+    }
 }
 class Product {
 	private String name;
 	private double price;
 	private int amount; //Amount in storage
 	public Product(String name, double price, int amount) {
-	this.name = name;
-	this.price = price;
-	this.amount = amount;
-}
-public double countValue() {
-	return(amount * price);
-}
+		this.name = name;
+		this.price = price;
+		this.amount = amount;
+	}
+	public double countValue() {
+		return(amount * price);
+	}
 public void printProduct() {
 	System.out.printf("Product %s, price %4.1f and balance %d pcs",name,price,amount);
 	}
